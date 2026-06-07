d5cd97b9ba03e97d33f67b0a0c3ab9fbdcc0a0ee
SIDDHARTH <121356793+DevSidX@users.noreply.github.com>
2026-06-07 13:58:23 +0530

    -add logo component and dropdown menu; -update form handling with react-hook-form - Implemented a dropdown menu component utilizing Radix UI for enhanced UI interactions. - form utility component

---
+++ Frontend/src/components/logo/index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+import logoSvg from "../../assets/chat_logo.svg"
+import { cn } from "@/lib/utils"
+
+interface LogoProps {
+    url?: string
+    showText?: boolean
+    imgClass?: string
+    textClass?: string
+}
+
+const Logo = ({
+    url = "/",
+    showText = true,
+    imgClass = "size-[30px]",
+    textClass
+}: LogoProps) => {
+    return (
+        <Link to={url} className="flex items-center gap-2 w-fit">
+            <img src={logoSvg} alt="Talkio Logo" className={cn(imgClass)} />
+
+            {showText &&
+                (<span className={cn("font-semibold text-lg leading-tight", textClass)}>
+                    Talkio
+                </span>)
+            }
+        </Link>
+    )
+}
+
+export default Logo
